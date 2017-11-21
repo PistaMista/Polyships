6f9b653752d8088590e0ee72223d814efd4e43c0
PistaMista-Desktop <krystofalbrecht@icloud.com>
2017-11-21 21:48:59 +0100

    Fixed input on mobile devices (maybe).

--- Assets/Scripts/User Interface/InputEnabledUserInterface_Master.cs
+++ Assets/Scripts/User Interface/InputEnabledUserInterface_Master.cs
@@ -11,16 +11,6 @@ public class InputEnabledUserInterface_Master : InputEnabledUserInterface
 
     protected override void ProcessInput()
     {
-        if (Application.isEditor)
-        {
-            PCInput();
-        }
-        else
-        {
-            MobileInput();
-        }
-
-
         Shared();
         base.ProcessInput();
 
@@ -32,64 +22,76 @@ public class InputEnabledUserInterface_Master : InputEnabledUserInterface
         lastFrameInputPosition = currentInputPosition;
     }
 
-    void PCInput()
-    {
-        currentInputPosition.screen = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    // void PCInput()
+    // {
+    //     currentInputPosition.screen = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
-        if (beginPress)
-        {
-            beginPress = false;
-        }
-        else
-        {
-            beginPress = Input.GetMouseButtonDown(0);
-        }
+    //     if (beginPress)
+    //     {
+    //         beginPress = false;
+    //     }
+    //     else
+    //     {
+    //         beginPress = Input.GetMouseButtonDown(0);
+    //     }
 
-        if (endPress)
-        {
-            endPress = false;
-        }
-        else
-        {
-            endPress = Input.GetMouseButtonUp(0);
-        }
+    //     if (endPress)
+    //     {
+    //         endPress = false;
+    //     }
+    //     else
+    //     {
+    //         endPress = Input.GetMouseButtonUp(0);
+    //     }
 
-        pressed = Input.GetMouseButton(0);
+    //     pressed = Input.GetMouseButton(0);
 
-        inputPoints = Input.GetKey(KeyCode.Space) ? 2 : 1;
-    }
+    //     inputPoints = Input.GetKey(KeyCode.Space) ? 2 : 1;
+    // }
 
+
+    //bool lastState;
+    // void MobileInput()
+    // {
+    //     inputPoints = Input.touchCount;
+    //     Debug.Log(inputPoints);
+    //     if (inputPoints > 0)
+    //     {
+    //         pressed = true;
+    //         Touch touch = Input.GetTouch(0);
+    //         currentInputPosition.screen = new Vector3(touch.position.x, touch.position.y);
+    //     }
+    //     else
+    //     {
+    //         pressed = false;
+    //     }
+
+
+    //     beginPress = !lastState && pressed;
+    //     endPress = lastState && !pressed;
+
+    //     lastState = pressed;
+    // }
 
     bool lastState;
-    void MobileInput()
+    void Shared()
     {
-        inputPoints = Input.touchCount;
-        Debug.Log(inputPoints);
-        if (inputPoints > 0)
-        {
-            pressed = true;
-            Touch touch = Input.GetTouch(0);
-            currentInputPosition.screen = new Vector3(touch.position.x, touch.position.y);
-        }
-        else
-        {
-            pressed = false;
-        }
-
+        pressed = Input.GetMouseButton(0);
+        currentInputPosition.screen = Input.mousePosition;
+        
 
         beginPress = !lastState && pressed;
         endPress = lastState && !pressed;
+        tap = endPress && !dragging;
 
-        lastState = pressed;
-    }
-
-    void Shared()
-    {
         if (beginPress)
         {
             initialInputPosition.screen = currentInputPosition.screen;
         }
+        
+        
+        
 
-        tap = endPress && !dragging;
+        lastState = pressed;
     }
 }
