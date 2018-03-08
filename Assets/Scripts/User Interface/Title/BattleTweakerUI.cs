830fb437f18968cd950fa816e232a5fc3db84a39
PistaMista-Desktop <krystofalbrecht@icloud.com>
2018-03-08 21:56:28 +0100

    Flag editor renders pixels.

--- Assets/Scripts/User Interface/Title/BattleTweakerUI.cs
+++ Assets/Scripts/User Interface/Title/BattleTweakerUI.cs
@@ -22,12 +22,21 @@ public class BattleTweakerUI : TitleSlaveUI
     public override void OnTitleSetState(UIState state)
     {
         base.OnTitleSetState(state);
-        flags = new float[2][,,];
         if ((int)state >= 2)
         {
+            flags = new float[2][,,];
             for (int i = 0; i < 2; i++)
             {
                 flags[i] = new float[MiscellaneousVariables.it.flagResolution.x, MiscellaneousVariables.it.flagResolution.y, 3];
+                for (int x = 0; x < flags[i].GetLength(0); x++)
+                {
+                    for (int y = 0; y < flags[i].GetLength(1); y++)
+                    {
+                        flags[i][x, y, 0] = 1;
+                        flags[i][x, y, 1] = 1;
+                        flags[i][x, y, 2] = 1;
+                    }
+                }
             }
         }
     }
--- Assets/Scripts/User Interface/Title/Flag Customizer/FlagEditingAreaUIAgent.cs
+++ Assets/Scripts/User Interface/Title/Flag Customizer/FlagEditingAreaUIAgent.cs
@@ -1,9 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlagEditingAreaUIAgent : InputEnabledUI
 {
-    public float[][,,] flagColors;
+    public GameObject pixelParent;
+    public int currentPlayer = 0;
 
+    protected override void SetState(UIState state)
+    {
+        base.SetState(state);
+        if ((int)state >= 2)
+        {
+            currentPlayer = 0;
+            ConstructFlag();
+        }
+        SetInteractable((int)state >= 2);
+    }
+
+    void ConstructFlag()
+    {
+        float[,,] flag = BattleTweakerUI.flags[currentPlayer];
+        Vector2 pixelDimensions = new Vector2(rect.rect.width / flag.GetLength(0), rect.rect.height / flag.GetLength(1));
+        Vector2 beginningPos = -rect.rect.size / 2.0f + pixelDimensions / 2.0f;
+
+        Destroy(pixelParent);
+        pixelParent = new GameObject("Pixel Parent");
+        pixelParent.transform.SetParent(transform, false);
+
+        for (int x = 0; x < flag.GetLength(0); x++)
+        {
+            for (int y = 0; y < flag.GetLength(1); y++)
+            {
+                GameObject pixel = new GameObject("Pixel x: " + x + " y: " + y);
+                pixel.transform.SetParent(pixelParent.transform, false);
+                pixel.transform.localPosition = beginningPos + Vector2.Scale(pixelDimensions, new Vector2Int(x, y));
+
+                RawImage image = pixel.AddComponent<RawImage>();
+                image.color = new Color(flag[x, y, 0], flag[x, y, 1], flag[x, y, 2]);
+                image.rectTransform.sizeDelta = pixelDimensions;
+            }
+        }
+    }
+
+    protected override void ProcessInput()
+    {
+        base.ProcessInput();
+        Debug.Log(currentInputPosition.screen - (Vector2)rect.position);
+    }
 }
