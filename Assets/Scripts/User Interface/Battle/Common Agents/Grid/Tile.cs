6b4fd269677ddfa222a8bbec29cafb314a84f93b
PistaMista-Desktop <krystofalbrecht@icloud.com>
2018-03-21 17:02:07 +0100

    Tile graphic setting method.

--- Assets/Scripts/User Interface/Battle/Common Agents/Grid/Tile.cs
+++ Assets/Scripts/User Interface/Battle/Common Agents/Grid/Tile.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 namespace BattleUIAgents.Base
 {
+    [RequireComponent(typeof(Renderer))]
     public class Tile : WorldBattleUIAgent
     {
         public void SetMaterialAndColor(Material material, Color color)
         {
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.material = material;
+            MaterialPropertyBlock block = new MaterialPropertyBlock();
+            block.SetColor("Color", color);
 
+            renderer.SetPropertyBlock(block);
         }
     }
 }
